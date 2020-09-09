751ae3ace78ab8f2067bfed7a52abd4e26b01bda
KITT <58197108+donaldkaiserjr@users.noreply.github.com>
2020-09-09 16:28:48 -0700

    Update incident.py

--- incident.py
+++ incident.py
@@ -1,60 +1,52 @@
-class Incident:
-    
-    def __init__(self, offender_sex, incident_location, inc_city, inc_state, 
-    incident_type, date, celeb_fname, celeb_lname, offender_fname, offender_lname, 
-    offender_age, d_license, police_disp, officer_fname, officer_lname, police_dept):
-        self.offender_sex = offender_sex
-        self.incident_location = incident_location
-        self.inc_city = inc_city
-        self.inc_state = inc_state
-        self.incident_type = incident_type
-        self.date = date
-        self.celeb_fname = celeb_fname
-        self.celeb_lname = celeb_lname
+class Offender:
+    def __init__(self, offender_fname, offender_lname, off_sex, off_age, off_d_license, date):
         self.offender_fname = offender_fname
         self.offender_lname = offender_lname
-        self.offender_age = offender_age
-        self.d_license = d_license
-        #boolean: True if police were dispatched
-        self.police_disp = police_disp
-        self.officer_fname = officer_fname
-        self.officer_lname = officer_lname
-        self.police_dept = police_dept
+        self.off_sex = off_sex
+        self.off_age = off_age
+        self.off_d_license = off_d_license
+        self.date = date
 
     @property
     def offender_full_name(self):
         return '{} {}'.format(self.offender_fname, self.offender_lname)
 
-    @property
-    def celebrity_full_name(self):
-        return '{} {}'.format(self.celeb_fname, self.celeb_lname)
+    def __repr__(self):
+        return "Offender('{}', '{}', '{}', '{}', '{}', '{}')".format(self.offender_fname, self.offender_lname, self.off_sex, self.off_age, self.off_d_license, self.date)
+
+class Security(Offender):
     
+    def __init__(self, offender_fname, offender_lname, off_sex, off_age, 
+    off_d_license, date, officer_fname, officer_lname, officer_id, company, celebrity, incident_type, 
+    incident_location, incident_city, incident_state, police_disp):
+        
+        Offender.__init__(self, offender_fname, offender_lname, off_sex, off_age, off_d_license, date)
+        self.officer_fname = officer_fname
+        self.officer_lname = officer_lname
+        self.officer_id = officer_id
+        self.company = company
+        self.celebrity = celebrity
+        self.incident_type = incident_type
+        self.incident_location = incident_location
+        self.incident_city = incident_city
+        self.incident_state = incident_state
+        #boolean: True if police were dispatched
+        self.police_disp = police_disp
+
     @property
     def officer_full_name(self):
         return '{} {}'.format(self.officer_fname, self.officer_lname)
 
     def __repr__(self):
-        return "Incident('{}', '{}','{}', '{}', '{}', '{}', '{}', '{}', '{}', {}', '{}', '{}', '{}', '{}', '{}', '{}')".format(self.offender_sex, self.incident_location, self.inc_city, self.inc_state, self.incident_type, self.date, self.celeb_fname, self.celeb_lname, self.offender_fname, self.offender_lname, self.offender_age, self.d_license, self.police_disp, self.officer_fname, self.officer_lname, self.police_dept)
-
+        return "Security('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(self.officer_fname, self.officer_lname, self.officer_id, self.company, self.celebrity, self.incident_type, self.incident_location, self.incident_city, self.incident_state, self.police_disp)
+        
     def __str__(self):
-        return 'On {} {} {} was involved in {} by offender: {} {} at  LOCATION: {}   CITY: {}   STATE: {}'.format(self.date, self.celeb_fname, self.celeb_lname, self.incident_type, self.offender_fname, self.offender_lname, self.incident_location, self.inc_city, self.inc_state)
-
-    def __len__(self):
-        return len(self.offender_full_name)
-
-    def __len__(self):
-        return len(self.celebrity_full_name)
+        return 'On {} {} was involved in {} by offender: {} {} at  LOCATION: {}   CITY: {}   STATE: {}'.format(self.date, self.celebrity, self.incident_type, self.offender_fname, self.offender_lname, self.incident_location, self.incident_city, self.incident_state)
 
     def __len__(self):
         return len(self.officer_full_name)
- 
-class Celebrity(Incident):
 
-    def __init__(self, offender_sex, incident_location, inc_city, inc_state, 
-    incident_type, date, celeb_fname, celeb_lname, offender_fname, offender_lname, 
-    offender_age, d_license, police_disp, officer_fname, officer_lname, police_dept):
-        
-        Incident.__init__(self, offender_sex, incident_location, inc_city, 
-        inc_state, incident_type, date, celeb_fname, celeb_lname, 
-        offender_fname, offender_lname, offender_age, d_license, police_disp, 
-        officer_fname, officer_lname, police_dept)
+
+    
+ricky_martin1 = Security("Justin", "Newton", "male", 24, "B111233", 20190904, "Gary", "Lang", 9933448, "A1 Security", "Justin Bieber", "Stage Trespass", "Staples", "Los Angeles", "CA", True)
+print(repr(ricky_martin1))
